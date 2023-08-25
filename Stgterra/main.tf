2242949b9141ab1bab1f6d37650de2cb68edcc7f
girish <dgirish2020@gmail.com>
2023-08-25 11:42:36 +0530

    modifies files are commited

--- Stgterra/main.tf
+++ Stgterra/main.tf
@@ -44,4 +44,11 @@ sku {
 }
 }
 
+rsource "azurerm_virtual_network" "vnet" {
+  name = "vnet"
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space = ["10.10.1.0"]
+}
+
 
